6886517375bdda6d5e11de38c81b10600447b0ec
Hallie Stiv <hstiv@e2r8p3.21-school.ru>
2019-03-24 13:32:58 +0300

    beginning

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hstiv <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/05 19:14:04 by hstiv             #+#    #+#             */
+/*   Updated: 2018/12/19 20:07:43 by hstiv            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	if (!f || !s)
+		return (NULL);
+	if (!(str = ft_strnew(ft_strlen((char *)s))))
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	return (str);
+}
